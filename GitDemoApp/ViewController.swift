3c8b2ae572efb65ba4c8958916c40f1b5c5df51e
AmreenKhan1003 <amreen10032000@gmail.com>
2022-09-19 01:44:54 -0700

    Initial Commit

---
+++ GitDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemoApp
+//
+//  Created by Capgemini-DA322 on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
